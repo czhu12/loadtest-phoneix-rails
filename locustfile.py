3f253d2bddaf68820614584e37d864fc721d2a5e
Your Name <you@example.com>
2015-05-30 12:39:24 -0400

    updated prod config

--- locustfile.py
+++ locustfile.py
@@ -13,7 +13,7 @@ class MessageTasks(TaskSet):
     })
   
 class WebsiteUser(HttpLocust):
-  host = "http://127.0.0.1:7000"
+  host = "http://127.0.0.1:4000"
   min_wait = 2000
   max_wait = 2500
   task_set = MessageTasks
