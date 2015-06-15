1476a9986f24dce254d53f62920494e759abe9e7
Your Name <you@example.com>
2015-06-15 03:31:43 -0400

    added caching

--- rails_messages/app/controllers/messages_controller.rb
+++ rails_messages/app/controllers/messages_controller.rb
@@ -1,7 +1,33 @@
 class MessagesController < ApplicationController
   def index
-    messages = Message.all
-    render :json => messages
+    key = "messages-#{Message.count}-#{Message.maximum(:updated_at)}"
+    hit = Rails.cache.exist?(key)
+    if hit
+      render :json => Rails.cache.fetch(key)
+    else
+      messages = Message.all
+      messages_json = []
+
+      messages.each do |message|
+        message_json = Rails.cache.fetch("message-#{message.id}-#{message.updated_at}") do
+          message.to_json
+        end
+
+        messages_json << message_json
+      end
+
+      Rails.cache.write(key, messages_json)
+      render :json => messages_json
+    end
+  end
+
+  def show
+    message = Message.find(params[:id])
+    key = "message-#{message.id}-#{message.updated_at}"
+    message_json = Rails.cache.fetch(key) do
+      message.to_json
+    end
+    render :json=>message_json
   end
 
   def create
@@ -14,4 +40,35 @@ class MessagesController < ApplicationController
       render :json => message.errors
     end
   end
+
+  #def destroy
+  #  message = Message.find(params[:id])
+  #  if message.destroy
+  #    Rails.cache.delete("message-#{message.id}")
+  #  end
+  #  
+  #  render :json=>message.to_json
+  #end
+
+  #def show
+  #  message = Message.find(params[:id])
+  #  message_json = Rails.cache.fetch("message-#{message.id}") do
+  #    message.to_json
+  #  end
+  #  
+  #  render :json => message_json
+  #end
+
+  #def update
+  #  message = Message.find(params[:id])
+  #  if message.update_attributes(params[:message])
+  #    message_json = message.to_json
+  #    Rails.cache.delete("message-#{message.id}")
+
+  #    Rails.cache.write("message-#{message.id}", message_json)
+  #    render :json=>message_json
+  #  else
+  #    render :json=>message.errors
+  #  end
+  #end
 end
